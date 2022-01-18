50fcb2359125256bf6c3f82f8bcc93d4d566f68a
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-01-18 10:05:20 +0100

    Add exasol jdbc driver as provided dependency

--- project.clj
+++ project.clj
@@ -3,18 +3,19 @@
   :url "https://github.com/exasol/metabase-driver/"
   :min-lein-version "2.9.7"
 
-  :repositories {"Local Metabase" "file:maven_repository"}
+  :repositories {"Local Metabase" "file:maven_repository"
+                 "Exasol Maven Repo" "https://maven.exasol.com/artifactory/exasol-releases"}
 
   :aliases
   {"test"           ["with-profile" "+unit_tests" "test"]
    "clj-kondo-deps" ["clj-kondo" "--copy-configs" "--dependencies" "--lint" "$classpath" "--cache-dir" ".clj-kondo/.cache"]
-   "lint"           ["do" ["clj-kondo-deps"] ["clj-kondo" "--lint" "src" "test" "--cache-dir" ".clj-kondo/.cache"]]
-   }
+   "lint"           ["do" ["clj-kondo-deps"] ["clj-kondo" "--lint" "src" "test" "--cache-dir" ".clj-kondo/.cache"]]}
 
   :profiles
   {:provided
    {:dependencies [[org.clojure/clojure "1.10.0"]
-                   [metabase "0.41.6"]]}
+                   [metabase "0.41.6"]
+                   [com.exasol/exasol-jdbc "7.1.4"]]}
 
    :unit_tests
    {:test-paths     ^:replace ["test_unit"]}
