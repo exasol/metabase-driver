f13372cf61dd5a85a37d3209d21c7564cf0a7d3e
chπ <christoph.pirkl@gmail.com>
2022-01-18 11:25:31 +0100

    Add integration tests (#23)
    * #3 Add integration tests for basic query features
    
    * Add changelog entry

--- test/metabase/driver/exasol_test.clj
+++ test/metabase/driver/exasol_test.clj
@@ -4,7 +4,9 @@
             [metabase.test.util :as tu]
             [metabase.test.data :as td]
             [metabase.test.data.dataset-definitions :as dataset]
-            [metabase.test.data.exasol-dataset-definitions :as exasol-dataset]))
+            [metabase.test.data.exasol-dataset-definitions :as exasol-dataset]
+            [metabase.query-processor :as qp]
+            [metabase.query-processor-test :as qp.test]))
 
 (deftest timezone-id-test
   (mt/test-driver :exasol
@@ -27,3 +29,126 @@
                                      (mt/rows (mt/run-mbql-query "data_types" {:fields [$name $hash $interval_ytm $interval_dts $geo]
                                                                                :order-by [[:asc $row_order]]})))))))
 
+(deftest join-test
+  (mt/test-driver :exasol
+                  (testing "Exasol supports inner joins"
+                    (is (= [["Big Red"        "Bayview Brood"]
+                            ["Callie Crow"    "Mission Street Murder"]
+                            ["Carson Crow"    "Mission Street Murder"]
+                            ["Chicken Little" "Bayview Brood"]
+                            ["Gerald Goose"   "Green Street Gaggle"]
+                            ["Greg Goose"     "Green Street Gaggle"]
+                            ["McNugget"       "Bayview Brood"]
+                            ["Oliver Owl"     "Portrero Hill Parliament"]
+                            ["Orville Owl"    "Portrero Hill Parliament"]
+                            ["Paul Pelican"   "SoMa Squadron"]
+                            ["Peter Pelican"  "SoMa Squadron"]
+                            ["Russell Crow"   "Mission Street Murder"]]
+                           (mt/rows
+                            (qp/process-query
+                             (mt/dataset dataset/bird-flocks
+                                         (mt/mbql-query "bird"
+                                                        {:fields   [$name &f.flock.name]
+                                                         :joins    [{:source-table $$flock
+                                                                     :condition    [:= $flock_id &f.flock.id]
+                                                                     :strategy     :inner-join
+                                                                     :alias        "f"}]
+                                                         :order-by [[:asc $name]]})))))))))
+
+
+(deftest filter-test
+  (testing "Exasol supports filtering with conditions"
+    (mt/test-driver :exasol
+                    (is (= [[21 "PizzaHacker"          58 37.7441 -122.421 2]
+                            [23 "Taqueria Los Coyotes" 50 37.765  -122.42  2]]
+                           (mt/formatted-rows :venues
+                                              (mt/run-mbql-query venues
+                                                                 {:filter   [:and [:< $id 24] [:> $id 20] [:!= $id 22]]
+                                                                  :order-by [[:asc $id]]})))))))
+
+(deftest select-fields-test
+  (testing "Exasol supports selecting defined fields"
+    (mt/test-driver :exasol
+                    (is (= {:rows [["Red Medicine"                  1]
+                                   ["Stout Burgers & Beers"         2]
+                                   ["The Apple Pan"                 3]
+                                   ["Wurstküche"                    4]]
+                            :cols [(mt/col :venues :name)
+                                   (mt/col :venues :id)]}
+                           (mt/format-rows-by [str int]
+                                              (qp.test/rows-and-cols
+                                               (mt/run-mbql-query venues
+                                                                  {:fields   [$name $id]
+                                                                   :limit    4
+                                                                   :order-by [[:asc $id]]}))))))))
+
+(deftest numeric-expression
+  (testing "Exasol supports numeric expressions, e.g. with +"
+    (mt/test-driver :exasol
+                    (is (= [[1 "Red Medicine"                 4  10.0646 -165.374 3 5.0]
+                            [2 "Stout Burgers & Beers"        11 34.0996 -118.329 2 4.0]
+                            [3 "The Apple Pan"                11 34.0406 -118.428 2 4.0]
+                            [4 "Wurstküche"                   29 33.9997 -118.465 2 4.0]
+                            [5 "Brite Spot Family Restaurant" 20 34.0778 -118.261 2 4.0]]
+                           (mt/formatted-rows [int str int 4.0 4.0 int float]
+                                              (mt/run-mbql-query venues
+                                                                 {:expressions {:calculated-field [:+ $price 2]}
+                                                                  :limit       5
+                                                                  :order-by    [[:asc $id]]})))))))
+
+(deftest math-functions
+  (testing "Exasol supports math functions, e.g. POWER()"
+    (mt/test-driver :exasol
+                    (is (= [[3 9.0]
+                            [2 4.0]
+                            [2 4.0]]
+                           (mt/rows
+                            (mt/run-mbql-query venues
+                                               {:expressions {"test" [:power [:field-id $price] 2]}
+                                                :fields      [$price [:expression "test"]]
+                                                :limit       3
+                                                :order-by    [[:asc $id]]})))))))
+
+
+
+(deftest string-function-regexp
+  (testing "Exasol supports string functions, e.g. REGEX_MATCH_FIRST()"
+    (mt/test-driver :exasol
+                    (is (= [["Red Medicine" "Red"]
+                            ["Stout Burgers & Beers" nil]]
+                           (mt/rows
+                            (mt/run-mbql-query venues {:expressions {"test" [:regex-match-first [:field-id $name] "(.ed+)"]}
+                                                       :fields      [$name [:expression "test"]]
+                                                       :order-by    [[:asc [:field-id (td/id :venues :id)]]]
+                                                       :limit       2})))))))
+
+
+(deftest aggregation
+  (testing "Exasol supports aggregation"
+    (mt/test-driver :exasol
+                    (is (= {:cols [(qp.test/breakout-col :venues :price)
+                                   (qp.test/aggregate-col :cum-count :venues :id)]
+                            :rows [[1 22]
+                                   [2 81]
+                                   [3 94]
+                                   [4 100]]}
+                           (qp.test/rows-and-cols
+                            (mt/format-rows-by [int int]
+                                               (mt/run-mbql-query venues
+                                                                  {:aggregation [[:cum-count $id]]
+                                                                   :breakout    [$price]}))))))))
+
+(deftest nested-query
+  (testing "Exasol supports nested queries"
+    (mt/test-driver :exasol
+                    (is (= {:rows [[1 174] [2 474] [3 78] [4 39]]
+                            :cols [(qp.test/breakout-col (qp.test/fk-col :checkins :venue_id :venues :price))
+                                   (qp.test/aggregate-col :count)]}
+                           (qp.test/rows-and-cols
+                            (mt/format-rows-by [int int]
+                                               (mt/run-mbql-query checkins
+                                                                  {:source-query {:source-table $$checkins
+                                                                                  :filter       [:> $date "2014-01-01"]}
+                                                                   :aggregation  [:count]
+                                                                   :order-by     [[:asc $venue_id->venues.price]]
+                                                                   :breakout     [$venue_id->venues.price]}))))))))
