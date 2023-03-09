f85310d8f88baebbaf6affae93b2af0b212d5b9d
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-03-09 08:33:39 +0100

    #65: Adapt build scripts to Metabase v46
    See https://discourse.metabase.com/t/building-drivers-with-metabase-v46/24241/2

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,11 @@ source "$exasol_driver_dir/scripts/common.sh"
 
 patch_metbase_build_scripts
 
-log_info "Building exasol driver in $exasol_driver_dir..."
-cd "$exasol_driver_dir"
-clojure -X:build :project-dir "\"$(pwd)\""
+log_info "Building exasol driver in $exasol_driver_dir using Metabase $metabase_dir..."
+cd "$metabase_dir"
+DRIVER_PATH=$exasol_driver_dir
+clojure \
+  -Sdeps "{:aliases {:exasol {:extra-deps {com.metabase/exasol-driver {:local/root \"$DRIVER_PATH\"}}}}}" \
+  -X:build:exasol \
+  build-drivers.build-driver/build-driver! \
+  "{:driver :exasol, :project-dir \"$DRIVER_PATH\", :target-dir \"$DRIVER_PATH/target\"}"
