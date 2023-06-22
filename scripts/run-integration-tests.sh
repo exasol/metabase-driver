9d25437cf3ed0b08e5fdb35831fc1c830791323a
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-06-22 09:53:38 +0200

    #68: Add tests with Exasol v8 (#69)

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly jdbc_driver_version=7.1.19
+readonly jdbc_driver_version=7.1.20
 
 exasol_driver_dir="$( cd "$(dirname "$0")/.." >/dev/null 2>&1 ; pwd -P )"
 metabase_dir=$(cd "$exasol_driver_dir/../metabase"; pwd)
