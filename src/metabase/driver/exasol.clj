45424130a3160cbf55f23b8bfbac959e971a840c
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-10-06 10:49:12 +0200

    Explicitly add new features

--- src/metabase/driver/exasol.clj
+++ src/metabase/driver/exasol.clj
@@ -59,7 +59,9 @@
 
 (doseq [[feature supported?] {:set-timezone           true
                               :nested-fields          false
-                              :nested-field-columns   false}]
+                              :nested-field-columns   false
+                              :schemas                true
+                              :uploads                false}]
   (defmethod driver/database-supports? [:exasol feature] [_ _ _] supported?))
 
 (defmethod sql.qp/quote-style :exasol
