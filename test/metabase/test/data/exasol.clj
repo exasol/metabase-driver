a437460a6c07a6de7ba34748f1fd2b45996153d9
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-12-15 10:19:04 +0100

    Remove unnecessary code

--- test/metabase/test/data/exasol.clj
+++ test/metabase/test/data/exasol.clj
@@ -1,20 +1,17 @@
 (ns metabase.test.data.exasol
   (:require [clojure.java.jdbc :as jdbc]
             [clojure.set :as set]
-            [clojure.string :as str]
-            [metabase.db :as mdb]
             [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
             [metabase.driver.sql-jdbc.execute :as sql-jdbc.execute]
             [metabase.driver.sql-jdbc.sync :as sql-jdbc.sync]
-            [metabase.models :as model]
-            [metabase.test.data.impl :as data.impl]
             [metabase.test.data.interface :as tx]
             [metabase.test.data.sql :as sql.tx]
             [metabase.test.data.sql-jdbc :as sql-jdbc.tx]
             [metabase.test.data.sql-jdbc.execute :as execute]
             [metabase.test.data.sql-jdbc.load-data :as load-data]
-            [metabase.util :as u]
-            [toucan.db :as db]))
+            [metabase.util :as u]))
+
+(set! *warn-on-reflection* true)
 
 (sql-jdbc.tx/add-test-extensions! :exasol)
 
@@ -68,34 +65,6 @@
           session-schema
           (tx/db-qualified-table-name database-name table-name)))
 
-(defonce ^:private exasol-test-dbs-created-by-this-instance (atom #{}))
-
-(defn- destroy-test-database-if-created-in-different-session
-  [database-name]
-  (when-not (contains? @exasol-test-dbs-created-by-this-instance database-name)
-    (locking exasol-test-dbs-created-by-this-instance
-      (when-not (contains? @exasol-test-dbs-created-by-this-instance database-name)
-        (mdb/setup-db!)                 ; if not already setup
-        (when-let [existing-db (db/select-one model/Database :engine "exasol", :name database-name)]
-          (let [existing-db-id (u/the-id existing-db)
-                all-schemas    (db/select-field :schema model/Table :db_id existing-db-id)]
-            (when-not (= all-schemas #{session-schema})
-              (println
-               (str "[exasol] At least one table's schema for the existing '%s' Database"
-                    " (id %d), which include all of [%s], does not match current session-schema"
-                    " of %s; deleting this DB so it can be recreated")
-               database-name
-               existing-db-id
-               (str/join "," all-schemas)
-               session-schema)
-              (db/delete! model/Database :id existing-db-id))))
-        (swap! exasol-test-dbs-created-by-this-instance conj database-name)))))
-
-(defmethod data.impl/get-or-create-database! :exasol
-  [driver dbdef]
-  (let [{:keys [database-name], :as dbdef} (tx/get-dataset-definition dbdef)]
-    (destroy-test-database-if-created-in-different-session database-name)
-    ((get-method data.impl/get-or-create-database! :sql-jdbc) driver dbdef)))
 
 (defmethod sql.tx/create-db-sql :exasol [& _] nil)
 
@@ -120,7 +89,7 @@
   (sql-jdbc.conn/connection-details->spec :exasol (connection-details)))
 
 (defn- non-session-schemas
-  "Return a set of the names of schemas (users) that are not meant for use in this test session (i.e., ones that should
+  "Return a set of the names of schemas that are not meant for use in this test session (i.e., ones that should
   be ignored). (This is used as part of the implementation of `excluded-schemas` for the Exasol driver during tests.)"
   []
   (set (map :schema_name (jdbc/query (dbspec) ["SELECT schema_name FROM SYS.EXA_ALL_SCHEMAS WHERE schema_name <> ?" session-schema]))))
